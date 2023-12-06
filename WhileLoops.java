23fd093b209b349487c86efaba12bd2dde7167c0
nbailat <144830998+nbailat@users.noreply.github.com>
2023-12-06 13:59:13 -0500

    Add files via upload

--- WhileLoops.java
+++ WhileLoops.java
@@ -8,6 +8,17 @@ public class WhileLoops {
         System.out.println(loop.howManyYears(120, 150));
         System.out.println(loop.howManyYears(111.2, 120));
         System.out.println(loop.howManyYears(111.2, 150));
+        System.out.println(loop.sumDigits(73));
+        System.out.println(loop.sumDigits(1500000));
+        System.out.println(loop.sumDigits(756218));
+        System.out.println(loop.printSum(6));
+        System.out.println(loop.printSum(8));
+        System.out.println(loop.printSum(2));
+        System.out.println(loop.isPerfectSquare(25));
+        System.out.println(loop.isPerfectSquare(45));
+        System.out.println(loop.isPerfectSquare(5));
+        System.out.println(loop.isPerfectSquare(49));
+
     }   
     public int addOdds(int n){
         int sum = 0;
@@ -22,10 +33,12 @@ public class WhileLoops {
     public int sumDigits(int num){
         int sum = 0;
         int dig;
-        while(num%){
-        
-
+        while(num>0){
+        dig = num%10;
+        sum = sum +dig;
+        num = num/10;
         }
+        return sum;
     }
 
     public int howManyYears(double startpop, double endpop){
@@ -37,6 +50,26 @@ public class WhileLoops {
             pop = pop+pop*rate;
             years++;    
         }
+            System.out.print("Years:");
             return years;
+    }
+
+    public void printSum(int num){
+        int sum = 0;
+        int dig = 0;
+        while(num>0){
+            sum = sum+num;
+            dig = num-1;
+            System.out.print(dig + "+");
+        }
+            System.out.println("=" + sum);
+    }
+
+    public boolean isPerfectSquare(int n){
+        int dig = 1;
+        while(Math.sqrt(n)){
+            dig = dig+2;
+       
+        }    
     }
 }
