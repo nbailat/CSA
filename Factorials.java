87bd8ed4c21520bf18c6c77f8a8e34e65f6ee702
nbailat <144830998+nbailat@users.noreply.github.com>
2023-12-12 13:58:36 -0500

    Add files via upload

--- Factorials.java
+++ Factorials.java
@@ -3,18 +3,35 @@ public class Factorials {
     public static void main(String [] args){
         Factorials fact = new Factorials();
         System.out.println(fact.calcFactorial(6));
-        for(int n)
+        for(int factorial = 1; factorial <=20; factorial++){
+            System.out.println(fact.calcFactorial(factorial));
+        }
+        System.out.println(fact.calcE(6));
+    }
     
 
-    }
 
-    public int calcFactorial(int num){
+
+    public long calcFactorial(int num){
         int facts = num;
-        int factorial = 1;
+        long factorial = 1;
         while (facts>0){
             factorial = factorial*facts;
             facts = facts-1;
         }
         return factorial;
     }
+
+    public long calcE(long e){
+        int factor = 1.0;
+        long olde = e;
+        e = 1+(1.0/calcFactorial(factor));
+        while(e-olde > 0.001){
+            ++factor;
+        e = e + (1.0/calcFactorial(factor));
+        }
+        return e;
+    }
+
+
 }
