a455314a6b232d07186a52a5b967f6f479821219
nbailat <144830998+nbailat@users.noreply.github.com>
2023-12-14 13:33:10 -0500

    Add files via upload

--- Factorials.java
+++ Factorials.java
@@ -6,7 +6,12 @@ public class Factorials {
         for(int factorial = 1; factorial <=20; factorial++){
             System.out.println(fact.calcFactorial(factorial));
         }
-        System.out.println(fact.calcE(6));
+        System.out.println(fact.calcE());
+        System.out.printf("e is %2.3f \n", fact.calcEX(1));
+        System.out.printf("e is %2.3f \n", fact.calcEX(2));
+        System.out.printf("e is %2.3f \n", fact.calcEX(3));
+        System.out.printf("e is %2.3f \n", fact.calcEX(4));
+        System.out.printf("e is %2.3f \n", fact.calcEX(5));
     }
     
 
@@ -22,16 +27,35 @@ public class Factorials {
         return factorial;
     }
 
-    public long calcE(long e){
-        int factor = 1.0;
-        long olde = e;
+    public double calcE(){
+        double e = 1.0;
+        int factor = 1;
+        double olde = 0;
         e = 1+(1.0/calcFactorial(factor));
         while(e-olde > 0.001){
+            olde=e; 
             ++factor;
-        e = e + (1.0/calcFactorial(factor));
+            e = e + (1.0/calcFactorial(factor)); 
         }
         return e;
     }
 
+    public double calcEX(int x){
+        int pow = 1;
+        double e = 1.0;
+        int factor = 1;
+        double olde = 0;
+        e = 1.0+(Math.pow(x, pow)/calcFactorial(factor));
+        while(e-olde > 0.001){
+            olde=e; 
+            ++factor;
+            ++pow;
+            e = e + (Math.pow(x, pow)/calcFactorial(factor));
+            
+            
+    }
+        return e;
+
+    }
 
 }
