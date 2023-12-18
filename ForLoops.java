26d5ff7e9377bc9f91c4c858a755fbef02cb2c39
nbailat <144830998+nbailat@users.noreply.github.com>
2023-12-18 13:58:59 -0500

    Add files via upload

--- ForLoops.java
+++ ForLoops.java
@@ -4,6 +4,11 @@ public class ForLoops {
         ForLoops frlps = new ForLoops();
         System.out.println(frlps.addEvens(4));
         System.out.println(frlps.addEvens(9));
+        System.out.println(frlps.addEvens(12));
+        frlps.printSquaresBack(8);
+        frlps.sillyNumbers();
+        frlps.sillyNumbers2();
+        frlps.dollarsAndStars();
 
     }
 
@@ -15,4 +20,46 @@ public class ForLoops {
         }
         return sum;
     }
-}
+
+    public void printSquaresBack(int n){
+        for(int num =n; num>=1; num--){
+            System.out.print(num*num+" ");
+        }
+        
+    }
+
+    public void sillyNumbers(){
+        for(int i=1; i<=3; i++){
+            System.out.println();
+            for(int j=0; j<10; j++){
+                for(int s=1; s<=3;s++){
+                    System.out.print(j);
+                }
+                
+            }
+        }
+    }
+
+    public void sillyNumbers2(){
+        for(int i=0; i<=3; i++){
+            System.out.println();
+            for(int j=9; j>0; j--){
+                for(int s=1; s<=j;s++){
+                    System.out.print(j);
+                }
+
+            }
+        }
+    }
+
+    public void dollarsAndStars(){
+        for (int i =0; i<=7;i++){
+            System.out.println();
+            for(int j=12; j>=1; j--){
+                System.out.print("*");
+                }
+            }
+        }
+    }
+
+
