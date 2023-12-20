87dc316cb7f32d347d2df787e801ce6548bffbac
nbailat <144830998+nbailat@users.noreply.github.com>
2023-12-20 13:59:58 -0500

    Add files via upload

--- AdvForLoops.java
+++ AdvForLoops.java
@@ -3,15 +3,30 @@ public class AdvForLoops {
     public static void main (String [] args){
         AdvForLoops adv = new AdvForLoops();
         adv.eights();
+        adv.alternate(5);
     }
 
     public void eights(){
-        for (int e = 1; e>=4; e++){
-            for (int i = 8; i>=e; i= i-2){
-                for (int g=i; g>=i; g--){
-                    System.out.print(" ");
-                    System.out.println(i);
-                }
+        for (int i = 8; i>=2; i= i-2){
+            for (int g=8; g>i; g=g-2){
+                System.out.print(" ");
+            }
+            for (int h = 1; h<=i; h++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public void alternate(int n){
+        for (int a=1; a<=n; a++){
+            if (a%2==1){
+                for (int l=1; l<=n-1; l++)
+            }
+            for (int l=a; l>=1; a--){
+                for ()
+                
+                System.out.print(a);
             }
         }
     }
